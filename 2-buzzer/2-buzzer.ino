ed06e6c2051e0810fd25ffd8621c1291098b276e
Caroline Dunn <thecarolinedunn@gmail.com>
2025-11-06 11:19:44 -0500

    Update 2-buzzer.ino

--- 2-buzzer/2-buzzer.ino
+++ 2-buzzer/2-buzzer.ino
@@ -1,10 +1,11 @@
 /*
-  Reset Button Test with LCD Screen
+  Reset Button Test with LCD Screen and Sound
   - LCD shows "Ready" / "Press Reset Button" on boot
-  - When reset button pressed, LCD shows "Reset Button Pressed"
+  - When reset button pressed, LCD shows "Reset Button Pressed" and plays sound
   - LCD returns to ready message after 10 seconds
 
   Reset Button: D8 -> GND
+  Buzzer: D9
   I2C LCD: A4 (SDA), A5 (SCL), VCC 5V, GND
 */
 
@@ -13,6 +14,7 @@
 
 // ------------- CONFIG -------------
 const uint8_t PIN_BTN_RST = 8;
+const uint8_t PIN_BUZZER = 9;
 const unsigned long DEBOUNCE_MS = 30;
 const unsigned long DISPLAY_TIMEOUT_MS = 10000;  // 10 seconds
 
@@ -22,12 +24,32 @@ const uint8_t LCD_COLS = 16;
 const uint8_t LCD_ROWS = 2;
 
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
+
+// --------- Notes (Hz) -------------
+#define NOTE_C5  523
+#define NOTE_E5  659
+#define NOTE_G5  784
 // ----------------------------------
 
 // State
 unsigned long lastRstEdge = 0;
 unsigned long pressedAt = 0;
 bool showingPressed = false;
+
+// ---------- Sound helpers ----------
+void toneGap(int freq, int durMs, int gapMs) {
+  if (freq > 0) tone(PIN_BUZZER, freq, durMs);
+  delay(durMs);
+  noTone(PIN_BUZZER);
+  if (gapMs > 0) delay(gapMs);
+}
+
+// Start chime when reset pressed
+void playStartChime() {
+  toneGap(NOTE_C5, 90, 10);
+  toneGap(NOTE_E5, 90, 10);
+  toneGap(NOTE_G5, 120, 0);
+}
 
 // -------- LCD helpers --------
 void printCentered16(uint8_t row, const char* msg) {
@@ -54,11 +76,14 @@ void showPressed() {
   printCentered16(1, "Pressed");
   showingPressed = true;
   pressedAt = millis();
+  playStartChime();  // Play sound when reset pressed
 }
 // -----------------------------
 
 void setup() {
   pinMode(PIN_BTN_RST, INPUT_PULLUP);
+  pinMode(PIN_BUZZER, OUTPUT);
+  noTone(PIN_BUZZER);
 
   // LCD
   lcd.init();
@@ -79,7 +104,7 @@ void loop() {
   }
 
   // --- Auto-return to ready screen after timeout ---
-  if (showingPressed && (now - pressedAt >= DISPLAY_TIMEOUT_MS)) {
+  if (showingPressed && (millis() - pressedAt >= DISPLAY_TIMEOUT_MS)) {
     showReady();
   }
 }
